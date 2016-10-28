d22246d4248e5e1a157bae7c7bac89bb6ed9b6ca
Yuyi <liyuyi828@gmail.com>
2016-10-27 23:50:29 -0700

    Add problem description to minStepsToOne

---
+++ problems/minStepsToOne/minStepsToOne.js
@@ -0,0 +1,21 @@
+// source: unknown
+
+// Minimum Steps to One
+// Given a positive integer, you can perform any combination of 3 operation for a interger:
+// 1) Subtract 1 from it.
+// 2) If its divisible by 2, divide by 2. 
+// 3) If its divisible by 3, divide by 3.
+// Find the minimum number of steps that it takes get from N to 1
+
+// Input: Positive Integer N
+// Output:Integer
+
+// What is the time and space complexity for your solution? 
+// Can you make it more efficient? 
+
+const minStepsToOne = (num) => {
+  // Your solution here
+
+}
+
+module.exports = { minStepsToOne };
