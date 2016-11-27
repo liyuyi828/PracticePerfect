eab37e15604f9c8d1d3d92672653c6356b8eface
Yuyi <liyuyi828@gmail.com>
2016-11-26 22:58:50 -0800

    complete isUnique and isPermutation

--- Cracking_Coding_Interview/Ch1_arrayAndString.js
+++ Cracking_Coding_Interview/Ch1_arrayAndString.js
@@ -19,3 +19,42 @@ const isUnique = (str) => {
 
 // 1.2: Check Permutation
 // Given two strings, write a method to decide if one is a permuation of the other.
+
+const isPermutation = (str1, str2) => {
+  if(str1 === str2 || str1.length !== str2.length){
+    return false;
+  } else {
+    let map1 = stringToMap(str1);
+    let map2 = stringToMap(str2);
+    for(let [key, value] of map1){
+      if(value !== map2.get(key)){
+        return false; 
+      }
+    }
+    return true;
+  }
+
+  function stringToMap(str){
+    let map = new Map();
+    let key, curr 
+    for(let i = 0; i < str.length; i++){
+      key = str[i]; 
+      curr = map.get(key); 
+      if(curr){
+        map.set(key, curr + 1); 
+      } else {
+        map.set(key, 1); 
+      }
+    }
+    return map; 
+  }
+}
+
+// console.log(isPermutation('abc d', 'abc d'));
+// console.log(isPermutation('abcd ', 'd abc'));
+
+// 1.3: URLify
+// Write a method to replace all spaces in a string with "%20". You may assume
+// that the string has sufficient space at the end to hold the additional characers, 
+// and that you are given the "true" length of the string.
+
