1bd018d103d0c7d3484146cc10552f5d12534e91
Yuyi <liyuyi828@gmail.com>
2016-11-29 20:40:57 -0800

    complete matrix rotation counter-clockwise

--- Cracking_Coding_Interview/Ch1_arrayAndString.js
+++ Cracking_Coding_Interview/Ch1_arrayAndString.js
@@ -182,4 +182,5 @@ const rotateMatrix = matrix => {
   }
   return matrix; 
 }
-console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+// console.log(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+// console.log(rotateMatrix([[1, 2, 3, 4, 5], [11, 12, 13, 14, 15], [21, 22, 23, 24, 25], [31, 32, 33, 34, 35], [41, 42, 43, 44, 45]]));
